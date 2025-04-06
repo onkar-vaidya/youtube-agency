b6c8010422b813f3c876e431b717ba1c931f7677
Omkar Vaidya <omkarvaidya166@gmail.com>
2025-04-07 00:18:13 +0530

    come new things

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ document.querySelectorAll("#mainNav ul li a").forEach(link => {
     document.getElementById("mainNav").classList.remove("show");
   });
 });
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const popup = document.getElementById("popupMessage");
+  popup.style.display = "block";
+  setTimeout(() => {
+    popup.style.display = "none";
+  }, 3000);
+  this.reset();
+});
